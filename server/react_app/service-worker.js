aa895713fca909d77f09e84abc63d2b5e8396dfb
Evan Tatay-Hinds <gaurdianaq@pop-os.localdomain>
2020-03-25 17:25:56 -0400

    Forgot to build the updated app

--- server/react_app/service-worker.js
+++ server/react_app/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f65992280a52ddf2f935dd0b3f2494c9.js"
+  "/precache-manifest.f5c98fcb71bce32af07a75e3f6d1526c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
