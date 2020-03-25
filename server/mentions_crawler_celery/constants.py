5578b5769ecdfa35def4b119e85bb7c38b25fbff
Evan Tatay-Hinds <gaurdianaq@pop-os.localdomain>
2020-03-25 19:17:41 -0400

    maybe this?

--- server/mentions_crawler_celery/constants.py
+++ server/mentions_crawler_celery/constants.py
@@ -6,7 +6,7 @@ FACEBOOK = "Facebook"
 
 _ISSUER = "Evan & Ryan"
 
-HOST = "http://localhost:"+os.environ["PORT"]
+HOST = "http://mentions-crawler.herokuapp.com"
 RESPONSE_URL = HOST+"/jobs/responses"
 COMPANIES_URL = HOST+"/companies"
 DB_CLEAN_URL = HOST+"/db/clean"
