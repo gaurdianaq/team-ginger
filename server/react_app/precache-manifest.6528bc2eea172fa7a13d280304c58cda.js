1dabdea2bbaa5f10267e05a3c13efe25a4caf37c
Evan Tatay-Hinds <gaurdianaq@pop-os.localdomain>
2020-03-25 15:08:33 -0400

    added deployment stuff

---
+++ server/react_app/precache-manifest.6528bc2eea172fa7a13d280304c58cda.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30728b1abc1829e69c720999e6f7b1ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5e4bb80d29867e194c3",
+    "url": "/static/css/main.7a6abedd.chunk.css"
+  },
+  {
+    "revision": "87d3c9b1cec913da511f",
+    "url": "/static/js/2.6cd7ea41.chunk.js"
+  },
+  {
+    "revision": "077433e387e1eddc892aa009ee83add0",
+    "url": "/static/js/2.6cd7ea41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5e4bb80d29867e194c3",
+    "url": "/static/js/main.6b6764e9.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "00d33694e0715fb1b68aa18e8ab0b467",
+    "url": "/static/media/reddit.00d33694.png"
+  },
+  {
+    "revision": "b89f0dfd46042cf3345bf725e8d30df2",
+    "url": "/static/media/twitter.b89f0dfd.png"
+  }
+]);
