ac090c55d741964188bdb30cc97f0e6672e1d355
Evan Tatay-Hinds <gaurdianaq@pop-os.localdomain>
2020-03-25 19:45:58 -0400

    one more try

--- server/mentions_crawler_flask/blueprints/job.py
+++ server/mentions_crawler_flask/blueprints/job.py
@@ -35,8 +35,8 @@ def requests(user, site_name: str):
         return ok_response("Task successfully cancelled!")
 
     else:
-        print(request.host_url)
-        result = enqueue(site_name, user_id, ("http://"+request.host_url), token)
+        host = "http://"+request.host_url
+        result = enqueue(site_name, user_id, host, token)
         if isinstance(result, AsyncResult):
             tasks[get_tasks_id(site_name, user_id)] = result
             return ok_response("Task successfully queued up!")
