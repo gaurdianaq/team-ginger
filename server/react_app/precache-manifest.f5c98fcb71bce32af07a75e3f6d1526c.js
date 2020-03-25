8c8c38d05c8c44cb8cdf6ec818c475f01525a633
Evan Tatay-Hinds <gaurdianaq@pop-os.localdomain>
2020-03-25 17:32:31 -0400

    apparently I forgot to commit stuff

---
+++ server/react_app/precache-manifest.f5c98fcb71bce32af07a75e3f6d1526c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7af0741593901a23dc8613cd3bd62461",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a7707dc0fcf6ecc3cc1",
+    "url": "/static/css/main.7a6abedd.chunk.css"
+  },
+  {
+    "revision": "87d3c9b1cec913da511f",
+    "url": "/static/js/2.6cd7ea41.chunk.js"
+  },
+  {
+    "revision": "077433e387e1eddc892aa009ee83add0",
+    "url": "/static/js/2.6cd7ea41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a7707dc0fcf6ecc3cc1",
+    "url": "/static/js/main.dde88647.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "00d33694e0715fb1b68aa18e8ab0b467",
+    "url": "/static/media/reddit.00d33694.png"
+  },
+  {
+    "revision": "b89f0dfd46042cf3345bf725e8d30df2",
+    "url": "/static/media/twitter.b89f0dfd.png"
+  }
+]);
